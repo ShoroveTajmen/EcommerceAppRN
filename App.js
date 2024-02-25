f06e21cfc440516eab57447081841a40402bf6c8
ShoroveTajmen <sorovitajmen@gmail.com>
2024-02-25 16:19:42 +0600

    dark and light mode toggle added

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
-import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { useColorScheme } from "nativewind";
+import { StatusBar, Switch, Text, View } from "react-native";
 
 export default function App() {
+  const { colorScheme, toggleColorScheme } = useColorScheme();
   return (
-    <View className="bg-orange-600 flex-1 items-center justify-center">
-      <Text className="rounded-full text-xl bg-white text-black p-6">
-        My name is Shorove
-      </Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 items-center justify-center bg-gray-200 dark:bg-black">
+      <View className="flex-row w-full gap-5">
+        <Text className="dark:text-white text-2xl font-bold">
+          New Collection
+        </Text>
+        <Switch
+          value={colorScheme === "dark"}
+          onChange={toggleColorScheme}
+        ></Switch>
+      </View>
+      <StatusBar style={colorScheme === "dark" ? "light" : "dark"} />
     </View>
   );
 }
